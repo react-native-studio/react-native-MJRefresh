c59e7df3ae0a261075d07f80e11c3b41e9213c6d
2534290808 <2534290808@qq.com>
2018-07-13 11:58:17 +0800

    update Example

--- Example/App.js
+++ Example/App.js
@@ -9,37 +9,20 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
-    Button,
+  View
 } from 'react-native';
-import HuaWeiRefreshControl from './HuaWeiRefreshControl'
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-import {ScrollView} from 'react-native-mjrefresh'
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-        <View style={{flex:1}}>
-        <ScrollView
-            //contentContainerStyle={{paddingVertical:50}}
-            style={{flex:1}}
-            scrollEventThrottle={16}
-            onScroll={e=>console.log(e.nativeEvent)}
-            refreshControl={<HuaWeiRefreshControl
-                ref={ref=>this._hw = ref}
-                onRefresh={()=>{
-                    setTimeout(()=>{
-                        this._hw.finishRefresh();
-                    },1000)
-                }}
-            />}
-        >
-
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -50,20 +33,17 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-        <Button title='beginRefresh' onPress={()=>{
-          this._hw.beginRefresh()
-        }}/>
       </View>
-        </ScrollView>
-        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-      height:1000
   },
   welcome: {
     fontSize: 20,
