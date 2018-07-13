be0ea87173b3e3fb29cfc6ff96d964aa05b7b266
2534290808 <2534290808@qq.com>
2018-07-13 10:35:50 +0800

    init 0.53

--- ios/RCTMJRefreshHeader/RCTMJRefreshHeader/RCTMJScrollContentShadowView.m
+++ ios/RCTMJRefreshHeader/RCTMJRefreshHeader/RCTMJScrollContentShadowView.m
@@ -1,8 +1,10 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
 #import "RCTMJScrollContentShadowView.h"
@@ -11,22 +13,44 @@
 
 #import <React/RCTUtils.h>
 
+@interface RCTShadowView () {
+    // This will be removed after t15757916, which will remove
+    // side-effects from `setFrame:` method.
+@public CGRect _frame;
+}
+@end
+
 @implementation RCTMJScrollContentShadowView
 
-- (void)layoutWithMetrics:(RCTLayoutMetrics)layoutMetrics
-            layoutContext:(RCTLayoutContext)layoutContext
+- (void)applyLayoutNode:(YGNodeRef)node
+      viewsWithNewFrame:(NSMutableSet<RCTShadowView *> *)viewsWithNewFrame
+       absolutePosition:(CGPoint)absolutePosition
 {
-    if (layoutMetrics.layoutDirection == UIUserInterfaceLayoutDirectionRightToLeft) {
-        // Motivation:
-        // Yoga place `contentView` on the right side of `scrollView` when RTL layout is enfoced.
-        // That breaks everything; it is completely pointless to (re)position `contentView`
-        // because it is `contentView`'s job. So, we work around it here.
-        
-        layoutContext.absolutePosition.x += layoutMetrics.frame.size.width;
-        layoutMetrics.frame.origin.x = 0;
+    // Call super method if LTR layout is enforced.
+    if (YGNodeLayoutGetDirection(self.yogaNode) != YGDirectionRTL) {
+        [super applyLayoutNode:node
+             viewsWithNewFrame:viewsWithNewFrame
+              absolutePosition:absolutePosition];
+        return;
     }
     
-    [super layoutWithMetrics:layoutMetrics layoutContext:layoutContext];
+    // Motivation:
+    // Yoga place `contentView` on the right side of `scrollView` when RTL layout is enfoced.
+    // That breaks everything; it is completly pointless to (re)position `contentView`
+    // because it is `contentView`'s job. So, we work around it here.
+    
+    // Step 1. Compensate `absolutePosition` change.
+    CGFloat xCompensation = YGNodeLayoutGetRight(node) - YGNodeLayoutGetLeft(node);
+    absolutePosition.x += xCompensation;
+    
+    // Step 2. Call super method.
+    [super applyLayoutNode:node
+         viewsWithNewFrame:viewsWithNewFrame
+          absolutePosition:absolutePosition];
+    
+    // Step 3. Reset the position.
+    _frame.origin.x = RCTRoundPixelValue(YGNodeLayoutGetRight(node));
 }
 
 @end
+
